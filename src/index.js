e4c42b817e1383635c73c3a2b0db746f33c7f8fa
Piero Narciso <pieronarciso988@gmail.com>
2020-11-09 16:51:40 -0500

    Project setup with command handler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 require('dotenv').config();
+const { prefix } = require('./config.json');
 
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Load commands
+const commandFiles = fs.readdirSync(__dirname + '/commands')
+                        .filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
 client.on('message', msg => {
-  if (msg.content === 'ping' && msg.channel.id === '775425712085532684') {
-    msg.reply('Pong!');
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).trim().split('/ +/');
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+
+  if (!command) return;
+
+  try {
+    command.execute(msg, args);
+  } catch(err) {
+    console.error(err);
+    msg.reply("Error trying to execute command!");
   }
 });
 
